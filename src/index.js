baa1780cee9a629d4b1aa531bff2f9aaf95af692
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-02-23 11:39:03 -0800

    Basic redux implemented

--- src/index.js
+++ src/index.js
@@ -11,19 +11,34 @@ import { createStore } from 'redux';
 const increment = () => {
   return {
     type: 'INCREMENT'
-  }
-}
+  };
+};
 
 const decrement = () => {
   return {
     type: 'DECREMENT'
-  }
-}
+  };
+};
 
 //REDUCER
+const counter = (state = 0, action) => {
+  switch(action.type) {
+    case "INCREMENT":
+      return state + 1;
+    case "DECREMENT":
+      return state - 1;
+  }
+};
+
+let store = createStore(counter);
+
+//Display it in the console
+store.subscribe(() => console.log(store.getState()));
 
 //DISPATCH
-
+store.dispatch(increment());
+store.dispatch(decrement());
+store.dispatch(decrement());
 
 ReactDOM.render(
   <React.StrictMode>
