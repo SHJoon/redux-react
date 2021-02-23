067dd200647d5f0aff53d235e2f986546109dcc7
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-02-23 14:25:40 -0800

    React redux finished

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { increment, decrement, toggle } from "./actions";
 
 function App() {
+  const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
-      
+      <h1>Counter {counter}</h1>
+
+      <button onClick={() => dispatch(increment(5))}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(toggle())}>Login/Logoff Toggle</button>
+
+      {isLogged ? <h1>Valueable information I shouldn't see</h1> : ""}
     </div>
   );
 }
