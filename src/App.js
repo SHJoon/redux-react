601947109499fc22f7828bf15c0b26df6c7a379b
SHJoon <52848941+SHJoon@users.noreply.github.com>
2021-03-09 18:19:38 -0800

    Additional testing

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
       <h1>Counter {counter}</h1>
 
       <button onClick={() => dispatch(increment(5))}>+</button>
-      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(decrement(3))}>-</button>
       <button onClick={() => dispatch(toggle())}>Login/Logoff Toggle</button>
 
       {isLogged ? <h1>Valueable information I shouldn't see</h1> : ""}
